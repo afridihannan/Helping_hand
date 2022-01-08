271785ddc35c2f8d8812148b5a0ae230e44c9461
afridihannan <afridihannan325@gmail.com>
2022-01-08 16:31:51 +0530

    Voice

---
+++ Extensions/Voice/alan.js
@@ -0,0 +1,9 @@
+var alanBtnInstance = alanBtn({
+    key: "c52d4c2eb75bb0d0761013d776327c3b2e956eca572e1d8b807a3e2338fdd0dc/stage",
+    onCommand: function (commandData) {
+      if (commandData.command === "go:back") {
+        
+      }
+    },
+    rootEl: document.getElementById("alan-btn"),
+  });
