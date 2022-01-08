0d87a289fa53a0af5d650b245d280fde03800847
afridihannan <afridihannan325@gmail.com>
2022-01-08 17:00:40 +0530

    Manifest

---
+++ Extensions/sketch/sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+    var c=createCanvas(windowWidth,windowHeight);
+    c.position(0,0);
+    clear();
+}
+
+function draw(){
+    stroke(0);
+    strokeWeight(4);
+    if(mouseIsPressed){
+        line(mouseX,mouseY,pmouseX,pmouseY);
+    }
+}
